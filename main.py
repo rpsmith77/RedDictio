aa7f07125cef94248a06c91bb6d730565599a3d4
Ryan Smith <rpsmith77@gmail.com>
2021-11-16 16:22:12 -0500

    added pages to view posts and comments

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ class Subreddits(db.Model):
     """
         Tmp for nn
     """
-    subreddit_id = db.Column(db.Integer, primary_key=True)  # Primary Key (Unique ID for db)
+    subreddit_id = db.Column(db.INTEGER, primary_key=True)  # Primary Key (Unique ID for db)
     subreddit_name = db.Column(db.TEXT, nullable=False)  # Text entered by user
-    hate_level = db.Column(db.DECIMAL, nullable=False)
+    hate_level = db.Column(db.FLOAT, nullable=False)
     last_edited = db.Column(db.DATE)  # Date and time entered
     being_edited = db.Column(db.BOOLEAN)
 
@@ -24,13 +24,43 @@ class Subreddits(db.Model):
         return '<%r>' % self.subreddit_id
 
 
-subreddit_scores = Subreddits.query.order_by(Subreddits.last_edited).all()
+class Posts(db.Model):
+    subreddit_id = db.Column(db.INTEGER)
+    post_id = db.Column(db.TEXT, primary_key=True)
+    post_title = db.Column(db.TEXT)
+
+    def __repr__(self):
+        return '<%r>' % self.subreddit_id
+
+
+class Comments(db.Model):
+    subreddit_id = db.Column(db.INTEGER)
+    post_id = db.Column(db.TEXT)
+    comment_id = db.Column(db.TEXT, primary_key=True)
+    date_added = db.Column(db.DATE)
+    comment_text = db.Column(db.TEXT)
+    comment_hate = db.Column(db.FLOAT)
+
+    def __repr__(self):
+        return '<%r>' % self.subreddit_id
 
 
 @app.route('/', methods=["GET"])
 @app.route('/index')  # multiple routes to same page
 def home():
-    return render_template('index.html', subreddit_scores=subreddit_scores)
+    return render_template('index.html', subreddits=Subreddits.query.order_by(Subreddits.last_edited).all())
+
+
+@app.route('/post/<subreddit_id>', methods=['GET'])
+def subreddit_posts(subreddit_id):
+    return render_template('subreddit_posts.html', posts=Posts.query.order_by(Posts.post_title).filter(
+        Posts.subreddit_id == subreddit_id))
+
+
+@app.route('/comment/<post_id>', methods=['GET'])
+def subreddit_comments(post_id):
+    return render_template('subreddit_comments.html', comments=Comments.query.order_by(Comments.comment_id).filter(
+        Comments.post_id == post_id))
 
 
 if __name__ == '__main__':
