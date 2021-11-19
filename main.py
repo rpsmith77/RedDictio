85c3291ea8dc50c800a1aa7a514b377346a8878d
Ryan Smith <rpsmith77@gmail.com>
2021-11-19 14:49:57 -0500

    added post hate levels

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 """
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///RedditDB.db'
@@ -12,12 +11,12 @@ db = SQLAlchemy(app)
 
 class Subreddits(db.Model):
     """
-        Tmp for nn
+        Table Subreddits from RedditDB.db
     """
     subreddit_id = db.Column(db.INTEGER, primary_key=True)  # Primary Key (Unique ID for db)
     subreddit_name = db.Column(db.TEXT, nullable=False)  # Text entered by user
     hate_level = db.Column(db.FLOAT, nullable=False)
-    last_edited = db.Column(db.DATE)  # Date and time entered
+    last_edited = db.Column(db.DATE)
     being_edited = db.Column(db.BOOLEAN)
 
     def __repr__(self):
@@ -28,6 +27,7 @@ class Posts(db.Model):
     subreddit_id = db.Column(db.INTEGER)
     post_id = db.Column(db.TEXT, primary_key=True)
     post_title = db.Column(db.TEXT)
+    hate_level = db.Column(db.FLOAT)
 
     def __repr__(self):
         return '<%r>' % self.subreddit_id
@@ -45,10 +45,10 @@ class Comments(db.Model):
         return '<%r>' % self.subreddit_id
 
 
-subreddits = Subreddits.query.order_by(Subreddits.last_edited).all()
+subreddits = Subreddits.query.order_by(Subreddits.subreddit_id).all()
 
 
-@app.route('/', methods=["GET"])
+@app.route('/', methods=['GET'])
 @app.route('/index')  # multiple routes to same page
 def home():
     return render_template('index.html', subreddits=subreddits)
@@ -56,14 +56,17 @@ def home():
 
 @app.route('/post/<subreddit_name>/<subreddit_id>', methods=['GET'])
 def subreddit_posts(subreddit_id, subreddit_name):
-    return render_template('subreddit_posts.html', posts=Posts.query.order_by(Posts.post_title).filter(
-        Posts.subreddit_id == subreddit_id), subreddit_name=subreddit_name)
+    return render_template('subreddit_posts.html',
+                           posts=Posts.query.order_by(Posts.post_title).filter(Posts.subreddit_id == subreddit_id,
+                                                                               Posts.hate_level is not None),
+                           subreddit_name=subreddit_name)
 
 
 @app.route('/comment/<post_id>/<post_title>', methods=['GET'])
 def subreddit_comments(post_id, post_title):
-    return render_template('subreddit_comments.html', comments=Comments.query.order_by(Comments.comment_id).filter(
-        Comments.post_id == post_id), post_title=post_title)
+    return render_template('subreddit_comments.html',
+                           comments=Comments.query.order_by(Comments.comment_id).filter(Comments.post_id == post_id),
+                           post_title=post_title)
 
 
 if __name__ == '__main__':
